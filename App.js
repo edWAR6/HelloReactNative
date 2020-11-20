3592ecd29c58676e7ebbd3c9cdf615238389e95f
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2020-11-19 19:59:07 -0600

    Hello World project

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import {StyleSheet, Text, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import React from 'react';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
